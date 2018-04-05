1e195c2ad23dbf27b48af3eecbc316f4bbfa8bda
Josh Kushner <joshkushner1@gmail.com>
2018-04-04 20:26:06 -0400

    :zap: Add settings for Heroku Deployment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Web sockets settings for Heroku
+  config.web_socket_server_url = "wss://orders-up.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ['https://orders-up.herokuapp.com', 'http://orders-up.herokuapp.com']
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -19,7 +23,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
