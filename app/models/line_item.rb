10a448f01d70c083241d4e7ec28945c7e592c8fb
Josh Kushner <joshkushner1@gmail.com>
2018-04-26 21:00:52 -0400

    :bug: Validation does not erase current sale

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,4 +1,6 @@
 class LineItem < ApplicationRecord
 	belongs_to :order
 	belongs_to :item, -> { with_deleted }
+
+	delegate :name, :price, to: :item
 end
--- spec/features/recap_spec.rb
+++ spec/features/recap_spec.rb
@@ -8,19 +8,29 @@ feature 'Recap' do
 			item1 = create(:item, restaurant: restaurant)
 			item2 = create(:item, restaurant: restaurant)
 			item3 = create(:item, restaurant: restaurant)
+
 			order = create(:order, :completed, restaurant: restaurant)
-			order2 = create(:order, :completed, restaurant: restaurant, created_at: 1.day.ago)
 			line_item1 = create(:line_item, item: item1, order: order)
 			line_item2 = create(:line_item, item: item2, order: order)
 			line_item3 = create(:line_item, item: item3, order: order)
+
+			order2 = create(:order, :completed, restaurant: restaurant, created_at: 1.day.ago)
 			line_item4 = create(:line_item, item: item2, order: order2)
 			line_item5 = create(:line_item, item: item3, order: order2)
+
+			# Cancelled order does not affect the total, should only be order2's total
+			# in yesterday's total
+			order3 = create(:order, :cancelled, restaurant: restaurant, created_at: 1.day.ago)
+			line_item6 = create(:line_item, item: item2, order: order3)
+			line_item7 = create(:line_item, item: item3, order: order3)
 
 			sign_in(user)
 
 			visit recap_restaurant_path(restaurant)
 			
-			todays_date = Time.current.strftime('%m/%d/%Y')
+			todays_date = Time.current
+											.in_time_zone('Eastern Time (US & Canada)')
+											.strftime('%m/%d/%Y')
 			todays_recap_table = find_section(todays_date)
 			expect(page).to have_selector(todays_recap_table)
 
@@ -28,19 +38,15 @@ feature 'Recap' do
 				expect(page).to have_content(order.total)
 			end
 
-			yesterdays_date = 1.day.ago.strftime('%m/%d/%Y')
+			yesterdays_date = 1.day.ago
+													.in_time_zone('Eastern Time (US & Canada)')
+													.strftime('%m/%d/%Y')
 			yesterdays_recap_table = find_section(yesterdays_date)
 			expect(page).to have_selector(yesterdays_recap_table)
 
 			within yesterdays_recap_table do
 				expect(page).to have_content(order2.total)
 			end
-			# This test should include a cancelled or incomplete order, which
-			# should not show up for that day on the recaps page
-		end
-
-		scenario 'each day has the correct order total' do
-			# add a data-section to each table that equals the date
 		end
 	end
 end
